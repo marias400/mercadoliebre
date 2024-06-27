d276d953698535e0f6aee412b048269480c3b8f7
marias400 <nano.arias12@gmail.com>
2024-06-27 05:19:03 -0300

    Se agrega paginas de registro y login
    Se agregan las vistas con sus respectivas hojas de estilo

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const PORT = 3030;
+const PORT = 80;
 const path = require('node:path');
 const express = require('express');
 const app = express();
@@ -9,10 +9,18 @@ app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
 });
 
-app.get("views/home.html", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.resolve("views/home.html"));
 });
 
-app.get("views/register.html", (req, res) => {
+app.get("/views/home.html", (req, res) => {
+    res.sendFile(path.resolve("views/home.html"));
+});
+
+app.get("/views/login.html", (req, res) => {
+    res.sendFile(path.resolve("views/login.html"));
+});
+
+app.get("/views/register.html", (req, res) => {
     res.sendFile(path.resolve("views/register.html"));
 });
